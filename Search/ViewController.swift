0a18e4e4757343b16eec7777e05cac749ac4ba62
Guanyi Fang <guanyi@mini1.local>
2015-11-02 02:38:46 -0800

    add scope bar, need add category array

--- Search/ViewController.swift
+++ Search/ViewController.swift
@@ -13,10 +13,20 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
+    //boolean variable to indicate if there is a seach action or not
     var searchActive: Bool = false
+    
+    //data
     private let dwarves = ["sleepy", "sneezy", "Bashful", "Happy", "Doc", "Grumpy", "Dopey", "Thorin", "Dorin", "Nori", "Ori", "Barlin", "Dwalin", "Fili", "Kili", "Oin", "Gloin", "Bifur", "Bofur", "Bombur"]
+    
+    //filtered data array
     var filteredDwarves: [String] = []
+    
     let simpleTableIdentifier = "SimpleTableIdentifier"
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if (searchActive) {
@@ -42,25 +52,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     
-    //Implemented for UISearchBarDelegate---------------------------------------
-    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
-        filteredDwarves = dwarves.filter({ (text) -> Bool in
-            let tmp: NSString = text
-            let range = tmp.rangeOfString(searchText, options: .CaseInsensitiveSearch)
-            return range.location != NSNotFound
-        })
-        
-        if (filteredDwarves.count == 0) {
-            searchActive = false
-        } else {
-            searchActive = true
-        }
-        self.tableView.reloadData()
-    }
-    
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
+    //Implemented for UISearchBarDelegate***************************************
     
     func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
         searchActive = true
@@ -77,7 +69,48 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func searchBarSearchButtonClicked(searchBar: UISearchBar) {
         searchActive = false
     }
-    //Implemented for UISearchBarDelegate---------------------------------------
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        filteredDwarves = dwarves.filter({ (text) -> Bool in
+            let tmp: NSString = text
+            let range = tmp.rangeOfString(searchText, options: .CaseInsensitiveSearch)
+            return range.location != NSNotFound
+        })
+        
+        if (filteredDwarves.count == 0) {
+            searchActive = false
+        } else {
+            searchActive = true
+        }
+        self.tableView.reloadData()
+    }
+    
+    /*Implement this function for settting scope bar filter. Scope bar buttons are 
+      represented by an integer index from 0 , 1, 2 ... GUI view buttom from left to
+      right respectively. Update the filtered name list according to scope bar
+      constrain, then let the table view reloadData()
+    */
+    func searchBar(searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        
+        //if name length is more then 4 characters, it is a long name, otherwise, it is a short name
+        switch selectedScope {
+        case 0:
+            filteredDwarves = dwarves.filter({ (text) -> Bool in
+                return text.characters.count > 4
+            })
+        case 1:
+            filteredDwarves = dwarves.filter({ (text) -> Bool in
+                return text.characters.count <= 4
+            })
+        default:
+            filteredDwarves = dwarves
+        }
+        searchActive = true
+        self.tableView.reloadData()
+    }
+
+    //Implemented for UISearchBarDelegate***************************************
 
 
     override func viewDidLoad() {
@@ -93,7 +126,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 

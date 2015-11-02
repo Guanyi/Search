e44b06c0c6cae69554e6556579e92de3d9eb32e7
Guanyi Fang <guanyi@mini1.local>
2015-11-02 01:41:23 -0800

    search bar done

--- Search/ViewController.swift
+++ Search/ViewController.swift
@@ -8,11 +8,85 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var searchActive: Bool = false
+    private let dwarves = ["sleepy", "sneezy", "Bashful", "Happy", "Doc", "Grumpy", "Dopey", "Thorin", "Dorin", "Nori", "Ori", "Barlin", "Dwalin", "Fili", "Kili", "Oin", "Gloin", "Bifur", "Bofur", "Bombur"]
+    var filteredDwarves: [String] = []
+    let simpleTableIdentifier = "SimpleTableIdentifier"
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (searchActive) {
+            return filteredDwarves.count
+        } else {
+            return dwarves.count
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier(simpleTableIdentifier)
+        
+        if (cell == nil) {
+            cell = UITableViewCell(style: .Default, reuseIdentifier: simpleTableIdentifier)
+        }
+
+        if (searchActive) {
+            cell?.textLabel!.text = filteredDwarves[indexPath.row]
+        } else {
+            cell?.textLabel!.text = dwarves[indexPath.row]
+        }
+        return cell!
+    }
+    
+    
+    //Implemented for UISearchBarDelegate---------------------------------------
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredDwarves = dwarves.filter({ (text) -> Bool in
+            let tmp: NSString = text
+            let range = tmp.rangeOfString(searchText, options: .CaseInsensitiveSearch)
+            return range.location != NSNotFound
+        })
+        
+        if (filteredDwarves.count == 0) {
+            searchActive = false
+        } else {
+            searchActive = true
+        }
+        self.tableView.reloadData()
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        searchActive = true
+    }
+    
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        searchActive = false
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchActive = false
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchActive = false
+    }
+    //Implemented for UISearchBarDelegate---------------------------------------
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
